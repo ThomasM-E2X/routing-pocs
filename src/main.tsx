a0b1bde5adab44e12c8d049d2dab1342dd36fc91
ThomasM-E2X <thomas.martin@e2x.com>
2024-05-13 15:04:35 +0100

    remove react router boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import { RouterProvider } from "react-router-dom";
-//import router from "./react-router/Router.tsx";
-
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
